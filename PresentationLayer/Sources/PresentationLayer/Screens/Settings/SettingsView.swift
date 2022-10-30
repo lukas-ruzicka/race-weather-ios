3e84b58f54b4800e8af14adc59ad51ce46f4a57b
Lukáš Růžička <lukas.ruzicka@synetech.cz>
2022-10-30 09:39:15 +0100

    Added external feedback form

--- PresentationLayer/Sources/PresentationLayer/Screens/Settings/SettingsView.swift
+++ PresentationLayer/Sources/PresentationLayer/Screens/Settings/SettingsView.swift
@@ -12,22 +12,47 @@ public struct SettingsView: View {
     // MARK: - Properties
     @ObservedObject var viewModel: SettingsViewModel
 
+    @Environment(\.openURL) private var openURL
+
     // MARK: - Body
     public var body: some View {
         List {
-            Section {
-                ForEach(viewModel.serieItems, id: \.serie) { item in
-                    Toggle(item.serie.name, isOn: .init(get: { item.isChecked },
-                                                        set: { viewModel.onSerieItemChanged(to: $0, item: item) }))
-                }
-            } header: {
-                L10n.Settings.adjustSeries
-            }
+            adjustSeriesSection
+            feedbackSection
         }
         .foregroundColor(.Text.primary)
         .navigationTitle(L10n.Settings.title)
         .onAppear {
             viewModel.onAppear()
+        }
+    }
+}
+
+// MARK: - Subviews
+private extension SettingsView {
+
+    var adjustSeriesSection: some View {
+        Section {
+            ForEach(viewModel.serieItems, id: \.serie) { item in
+                Toggle(item.serie.name, isOn: .init(get: { item.isChecked },
+                                                    set: { viewModel.onSerieItemChanged(to: $0, item: item) }))
+            }
+        } header: {
+            L10n.Settings.adjustSeries
+        }
+    }
+
+    var feedbackSection: some View {
+        Section {
+            Button {
+                openURL(URL(string: L10n.Settings.Feedback.linkString)!)
+            } label: {
+                L10n.Settings.Feedback.actionLabel
+            }
+        } header: {
+            L10n.Settings.Feedback.title
+        } footer: {
+            L10n.Settings.Feedback.footnote
         }
     }
 }
